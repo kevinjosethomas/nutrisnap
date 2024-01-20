9ecbcfe2b9ade8e4f9c241c01fede93451dba9b0
Isaac <isaacfungg@gmail.com>
2024-01-20 15:47:20 -0800

    added additional info for globalstate

--- frontend/context/GlobalState.js
+++ frontend/context/GlobalState.js
@@ -3,6 +3,10 @@ import AppReducer from './AppReducer';
 
 const initialState = {
     pastScans: [],
+    age: undefined,
+    weight: undefined,
+    dietaryRestrictions: undefined,
+    calorieTarget: undefined,
 }
 
 const GlobalContext = createContext(initialState);
@@ -17,9 +21,45 @@ export const GlobalProvider = ({ children }) => {
         });
     }
 
+    function setAge(age) {
+        dispatch({
+            type: 'SET_AGE',
+            payload: age,
+        })
+    }
+
+    function setDietaryRestrictions(restrictions) {
+        dispatch({
+            type: 'SET_DIETARY_RESTRICTIONS',
+            payload: restrictions,
+        })
+    }
+
+    function setWeight(weight) {
+        dispatch({
+            type: 'SET_WEIGHT',
+            payload: weight,
+        })
+    }
+
+    function setCalorieTarget(calorieTarget) {
+        dispatch({
+            type: 'SET_CALORIE_TARGET',
+            payload: calorieTarget,
+        })
+    }
+
     return (
         <GlobalContext.Provider value = {{
+            age: state.age,
+            weight: state.weight,
+            dietaryRestrictions: state.dietaryRestrictions,
+            calorieTarget: state.calorieTarget,
             pastScans: state.pastScans,
+            setAge,
+            setDietaryRestrictions,
+            setWeight,
+            setCalorieTarget,
             addScan,
         }}>
             {children}
