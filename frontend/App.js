b8eb5221795f21fdf96b74532cd26e8448eb122a
Isaac <isaacfungg@gmail.com>
2024-01-20 16:49:14 -0800

    Moved Tabs to its own file

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,8 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet} from 'react-native';
 import { GlobalProvider } from './context/GlobalState';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ProfileScreen, CameraScreen } from './screens';
-import { Platform } from 'react-native';
+import Tabs from './navigation/Tabs';
 
 
 export default function App() {
@@ -12,65 +11,7 @@ export default function App() {
   return (
     <GlobalProvider>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions = {{
-            tabBarInactiveTintColor: 'grey',
-            tabBarActiveTintColor: 'black',
-            headerShown: false,
-            tabBarLabelStyle: {
-              fontSize: 11,
-              fontFamily: Platform.OS === 'android' ? 'Roboto' : 'System',
-              fontWeight: 'bold',
-            }
-          }}
-        >
-          <Tab.Screen 
-            name="Past Scans"
-            component={ProfileScreen}
-            options = {{
-              title: "Past Scans",
-              tabBarIcon: () => (
-                <Image 
-                  source={require("./assets/PastScanTab.png")}
-                  style={{height: 30, width: 30}}
-                />
-              )
-
-            }}
-          />
-
-          {/* Profile Screen */}
-          <Tab.Screen 
-            name="Scrn"
-            component={CameraScreen}
-            options = {{
-              title: "Scan",
-              tabBarIcon: () => (
-                <Image 
-                  source={require("./assets/CameraTab.png")}
-                  style={{height: 25, width: 25}}
-                />
-              )
-
-            }}
-          />
-
-          <Tab.Screen 
-            name="Account"
-            component={ProfileScreen}
-            options = {{
-              title: "Account",
-              tabBarIcon: () => (
-                <Image 
-                  source={require("./assets/ProfileTab.png")}
-                  style={{height: 24, width: 24}}
-                />
-              )
-            }}
-          />      
-
-
-        </Tab.Navigator>
+        <Tabs />
       </NavigationContainer>
     </GlobalProvider>
     
