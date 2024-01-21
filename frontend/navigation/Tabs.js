16963d4cf83a3279e21e9e3c2fdd6afc6eb9701a
Isaac <isaacfungg@gmail.com>
2024-01-20 17:31:40 -0800

    added custom tab navigator

--- frontend/navigation/Tabs.js
+++ frontend/navigation/Tabs.js
@@ -2,6 +2,28 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ProfileScreen, CameraScreen } from '../screens';
 import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
+
+const CustomTabBarButton = ({children, onPress}) => (
+    <TouchableOpacity
+        style={{
+            top: -20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            ...styles.shadow
+        }}
+        onPress={onPress}
+    >
+        <View style={{
+            width: 70,
+            height: 70,
+            borderRadius: 35,
+            backgroundColor: '#e32f45', 
+        }}>
+            {children}
+        </View>
+    </TouchableOpacity>
+)
+
 export default function Tabs() {
     const Tab = createBottomTabNavigator();
 
@@ -23,31 +45,52 @@ export default function Tabs() {
             }
         }}
     >
-          <Tab.Screen 
-            name="Past Scans"
-            component={ProfileScreen}
-            options = {{
-              title: "Past Scans",
-              tabBarIcon: ({ focused }) => (
-                <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
-                   <Image 
-                        source={require("../assets/PastScanTab.png")}
-                        resizeMode='contain'
-                        style={{
-                            width: 25,
-                            height: 25,
-                            tintColor: focused ? '#e32f45' : "#748c94"
-                        }}
-                    /> 
-                    <Text style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12}}>
-                        PAST SCANS
-                    </Text>
-                </View> 
-              )
-            }}
-          />
+        <Tab.Screen 
+        name="Past Scans"
+        component={ProfileScreen}
+        options = {{
+            title: "Past Scans",
+            tabBarIcon: ({ focused }) => (
+            <View style={{alignItems: 'center', justifyContent: 'center', top: 10}}>
+                <Image 
+                    source={require("../assets/PastScanTab.png")}
+                    resizeMode='contain'
+                    style={{
+                        width: 35,
+                        height: 35,
+                        tintColor: focused ? '#e32f45' : "#748c94"
+                    }}
+                /> 
+                <Text style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12}}>
+                    PAST SCANS
+                </Text>
+            </View> 
+            )
+        }}
+        />
 
         <Tab.Screen 
+            name="Scan" 
+            component={CameraScreen}
+            options={{
+                tabBarIcon: ({focused}) => (
+                    <Image 
+                        source={require("../assets/CameraTab.png")}
+                        resizeMode='contain'
+                        style={{
+                            width: 30,
+                            height: 30,
+                            tintColor: '#fff',
+                        }}
+                    />
+                ),
+                tabBarButton: (props) => (
+                    <CustomTabBarButton {...props} />
+                )
+            }}
+        />
+
+        {/* <Tab.Screen 
             name="Scan"
             component={CameraScreen}
             options = {{
@@ -69,7 +112,7 @@ export default function Tabs() {
                 </View> 
               )
             }}
-          />
+          /> */}
 
         <Tab.Screen 
             name="Account"
